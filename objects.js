b4b945b99ff1d2f8cb254f7937e54a62d9432c23
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-21 17:04:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {beatles: "Abbey Road"}
 function updatePlaylist(playlist,artistName,songTitle){
   playlist[artistName] = songTitle
 }
+
+function removeFromPlaylist(playlist,artistName){
+  delete playlist[artistName]
+}
